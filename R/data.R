881dab9da17834d2b401f47b8fd56381d16b11c7
averyrobbins1 <averyrobbins17@gmail.com>
2020-07-05 21:45:29 -0600

    initial set up

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' Avatar: The Last Airbender data
+#'
+#' A dataset containing the transcripts, writers, directors, and imdb ratings
+#' of the greatest animated tv show of all time - Avatar: The Last Airbender.
+#'
+#' @format A data frame with 13,385 rows and 12 variables:
+#' \describe{
+#'   \item{id}{<int> an id number for each row, essentially just a row number}
+#'   \item{book}{<chr> the name of each book, or season, in the series: Water,
+#'   Earth, and Fire}
+#'   \item{book_num}{<int> an id number for each book, 1-3}
+#'   \item{chapter}{<chr> the name of each chapter, or episode, in the series}
+#'   \item{chapter_num}{<int> an id number for each chapter}
+#'   \item{character}{<chr> every character who ever spoke in the series}
+#'   \item{full_text}{<chr> the words spoken by each character and some
+#'   additional scene descriptions}
+#'   \item{character_words}{<chr> the words spoken by each character}
+#'   \item{scene_description}{<chr> the descriptions of what is happening on
+#'   screen, often as the characters are talking}
+#'   \item{writer}{the writer, or writers, for each episode}
+#'   \item{director}{the director for each episode}
+#'   \item{imdb_rating}{imdb ratings on a scale of 1 to 10}
+#'
+#' }
+#' @source \url{https://avatar.fandom.com/wiki/Avatar:_The_Last_Airbender}
+#'  \url{https://www.imdb.com/list/ls079841896/}
+"appa"
---
+++ README.md
@@ -0,0 +1,27 @@
+
+# appa
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of appa is to provide an easy way to share Avatar: The Last Airbender
+transcript data.
+
+## Installation
+
+You can install appa from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("averyrobbins1/appa")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(appa)
+## basic example code
+```
+
